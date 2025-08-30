6fbc82d0cb38e5bac2972d7e180eabcc01532bc2
Muhammad Umer <m.umer.looka@gmail.com>
2025-08-30 23:54:07 +0000

    make a sun dial animation for shifting light to dark or dark to light

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -18,9 +18,9 @@ export function ThemeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button variant="outline" size="icon" className="relative overflow-hidden">
+           <Sun className="h-[1.2rem] w-[1.2rem] text-yellow-500 transition-all duration-500 transform-gpu dark:-translate-y-[150%] dark:opacity-0" />
+           <Moon className="absolute h-[1.2rem] w-[1.2rem] text-slate-500 transition-all duration-500 transform-gpu translate-y-[150%] opacity-0 dark:translate-y-0 dark:opacity-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
